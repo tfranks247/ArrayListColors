6bd8bb41103b16d5c4df4fd4cc7f5c88807ad05f
tfranks247 <tfranks247@gmail.com>
2019-10-24 16:55:50 -0400

    first push

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] Array =new int[10];//declaration and instantiation
+
+        for(int i=0;i<Array.length;i++){//length is the property of array
+
+        Array [i] = i+1;
+            System.out.println(Array[i]);
+        }
+    }
+}
