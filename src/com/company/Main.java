c11e3d8901cdd4f921b6801d0df6d2a324d51ee9
tfranks247 <tfranks247@gmail.com>
2019-10-25 13:27:12 -0400

    commit for Array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,34 @@
 package com.company;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
-        int [] Array =new int[10];//declaration and instantiation
-
-        for(int i=0;i<Array.length;i++){//length is the property of array
-
-        Array [i] = i+1;
-            System.out.println(Array[i]);
+        int[] myIntegers = getIntegers(5);
+        for(int i=0; i<myIntegers.length; i++){
+            System.out.println("Element " + i +", typed value was " + myIntegers[i]);
         }
+        System.out.println("The average is " + getAverage(myIntegers));
+    }
+
+    public static int[] getIntegers(int number){
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int [number];
+        for (int i=0; i<values.length; i++){
+            values[i] = scanner.nextInt();
+
+        }
+        return values;
+    }
+
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for( int i=0; i<array.length; i++){
+            sum += array[i];
+        }
+
+        return (double) sum / (double) array.length;
     }
 }
